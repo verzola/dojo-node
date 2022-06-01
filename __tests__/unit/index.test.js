d95abd102e5d9391bad9d5ee6b2be55a55e7368c
Gustavo Verzola <verzola@gmail.com>
2022-05-31 22:17:37 -0300

    fix wrong name

--- __tests__/unit/index.test.js
+++ __tests__/unit/index.test.js
@@ -2,8 +2,8 @@ import Home from "@pages/index";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
 
-describe("Calculator", () => {
-  it("renders a calculator", () => {
+describe("HomePage", () => {
+  it("renders the home page", () => {
     render(<Home />);
     expect(screen.getByTestId("homepage")).toBeInTheDocument();
   });
